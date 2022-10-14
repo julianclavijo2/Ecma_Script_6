e0feb88d85f31dc970d033115c0537a42e320520
julian <julian.r@cheil.com>
2022-10-14 16:01:26 -0500

    14 de octubre

--- src/es6/index.js
+++ src/es6/index.js
@@ -103,5 +103,21 @@ const listNames5 = name =>{
 const square = num => num * num; 
 
 
+const helloPromise = () =>{
+    return new Promise((resolve , reject )=>{
+        if (true) {
+            resolve('hey');
+        } else {
+            reject('Ups!!');
+        }
+    })
+}
+
+helloPromise()
+.then(response => console.log(response))
+.catch(error => console.log(error));
+
+
+
 
 
